057bdb2ba5aa54a1b6476b2d459bdbadfcb68759
Mikhaylov-Andrey-derek23 <40835141+Mikhaylov-Andrey-derek23@users.noreply.github.com>
2019-05-27 21:27:57 +0300

    Update test.js

--- test.js
+++ test.js
@@ -12,10 +12,12 @@ class Price extends React.Component{
 class Cours extends React.Component{
     render(){
         return(
-            <div className='course-item'>
+            <a  href={this.props.link} className='course-item'>
             <h2>{this.props.courseName}</h2>
             <p><Price price={this.props.coursePrice}/></p>
-            </div>
+            <img src={this.props.img}/>
+            <div>Нужен {this.props.cursplus}</div>
+            </a>
             
         )
         
@@ -34,19 +36,69 @@ class Nordic extends React.Component{
         super(props)
         this.state= {
             num : 0, 
-            cours : [
-                {
-                    name : 'React',
-                    price : 19000
-                },
-                {
-                    name: 'Java',
-                    price: 17900 
-                }
-            ]
+            // cours : [
+            //     {
+            //         name : 'React',
+            //         price : 19000,
+            //         activ: true
+            //     },
+            //     {
+            //         name: 'Java',
+            //         price: 17900, 
+            //         activ: false 
+            //     },
+            //     {
+            //         name: 'Python',
+            //         price: 30000,
+            //         activ: true 
+
+            //     }
+            // ]
+            cours : [],
+            isload: false 
         }
     }
+    componentDidMount(){
+        // setTimeout(()=>{
+        //     this.setState({
+        //         cours:[
+        //             {
+        //                         name : 'React',
+        //                         price : 19000,
+        //                         activ: true
+        //                     },
+        //                     {
+        //                         name: 'Java',
+        //                         price: 17900, 
+        //                         activ: false 
+        //                     },
+        //                     {
+        //                         name: 'Python',
+        //                         price: 30000,
+        //                         activ: true 
+            
+        //                     }
+        //         ], isload:true
+        //     })
+        // }, 2000)
 
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET',' /date.json');
+        xhr.send();
+        
+        xhr.addEventListener('load', ()=>{
+            const respon = JSON.parse(xhr.responseText);
+            console.log('parse')
+            this.setState({
+                cours : respon.data,
+                isload : true
+            })
+           
+
+        } )
+        
+        
+    }
     renderCours2(){
         this.state.cours.forEach((elem)=>{
             return(
@@ -61,11 +113,43 @@ class Nordic extends React.Component{
 
         )
     }
+
+    salesPrice(){
+        let price = this.state.cours;
+        price.forEach((val)=>{
+
+            val.price = val.price - val.price*0.1;
+        })
+        this.setState({cours : price})
+    }
     
+    Invers(){
+        let price = this.state.cours;
+        price.forEach((val)=>{
+
+            if (val.activ == true) {
+                val.activ = false
+            }else{
+                val.activ = true;
+            }
+            
+        })
+        this.setState({cours : price})
+    }
+
 
     countChange(){
     
             this.setState({num: this.state.num + 1});
+            let arCourse = this.state.cours;
+            arCourse.push({
+                name: "C++",
+                price: 50000,
+                activ: false
+            })
+            this.setState({cours : arCourse})
+            console.log(arCourse)
+            //this.state.cours.push({name: "C++", price : 50000})
     
     }
 
@@ -84,9 +168,25 @@ class Nordic extends React.Component{
         
     }
     render(){
+
+        const a1 = this.state.cours;
+        a1.forEach((val)=>{
+            if (val.reuired != null){
+                val.nameCours = [];
+                this.state.cours.forEach((val2)=>{
+                    if(val.reuired == val2.id){
+                        val.nameCours.push(val)
+                    }
+                })
+            }
+        })
+        console.log(a1)
+
         return (
             <div className='wrapper'>
-                <div className='nordic'>Hello, word!</div>
+                <div className='nordic'><h1>{
+                    !this.state.isload ? 'Загрузка' : 'Загрузка завершина'
+                }</h1></div>
                 <button onClick={()=>{this.handlerClick()}}>Click me!</button>
                 <button onClick={()=>{this.color()}}>Click color</button>
                 <div className='course.box'>{this.renderCours('React', ' 200 руб.')}
@@ -95,7 +195,19 @@ class Nordic extends React.Component{
                 <Counter num={this.state.num}/>
                <button onClick = {()=>this.countChange()}>+ Click</button>
                <button onClick = {()=>this.countChange2()}>- Click</button>
+               <button onClick = {()=>this.salesPrice()}>- Price -</button>
+               <button onClick = {()=>this.Invers()}>- Покажем что скрыто -</button>
                <div>{this.renderCours2()}</div>
+               <div>
+                    {
+                        this.state.cours.map((item, key)=>
+                           item.activ ? <Cours link={item.id} courseName={item.name} coursePrice={item.price} img={item.img} 
+
+                           /> : ''
+                        )
+                    }
+               </div>
+
             </div>
         )
     }
@@ -138,11 +250,42 @@ class BankCard extends React.Component{
     render(){
         return(
             <div className="Wrapper">
-            <Credit creditImg={'Credit'} creditNumber={'1234'} creditData={'21.21'} creditB={-222}/>
-            <Depozit depozitImg={'Depozit'} depozitNumber={'4321'} depozitData={'21.21'} depozitB={5646}/>  
+            <Nordic/>
+            {/* <Credit creditImg={'Credit'} creditNumber={'1234'} creditData={'21.21'} creditB={-222}/>
+            <Depozit depozitImg={'Depozit'} depozitNumber={'4321'} depozitData={'21.21'} depozitB={5646}/>   */}
             </div>
         )
     }
 }
 
-ReactDOM.render(<BankCard />, document.getElementById('root'));
+ReactDOM.render(<Nordic/>, document.getElementById('root'));
+
+{
+    "data" :[
+        {
+            "id" : "1",
+            "name" : "Веб разработчик",
+            "price" : 17000,
+            "activ" : true,
+            "img" : "/img/12.jpg",
+            "reuired" : null
+        },
+        {   
+            "id" : 2,
+            "name" : "React",
+            "price" : 23000,
+            "activ" : true,
+            "img" : "/img/12.jpg",
+            "reuired" : 1
+        },
+        {   
+
+            "id" : 3,
+            "name" : "Java",
+            "price" : 19000,
+            "activ" : true,
+            "img" : "/img/12.jpg",
+            "reuired" : 2
+        }
+    ]
+}
